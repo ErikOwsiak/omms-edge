a6aab387b1e8b7d469d1828c6e0d7b66e4e1911a
erik <erik@iotech.systems>
2023-01-13 13:23:43 +0100

    changes to reg. stream for power stats

--- modbus/modbusRedisRelay.py
+++ modbus/modbusRedisRelay.py
@@ -216,6 +216,7 @@ class modbusRedisRelay(_th.Thread):
                self.redops.save_meter_data(meter.syspath, _dict=d)
                # -- publish to redis channel --
                strs_arr.insert(0, "#RPT")
+               strs_arr.insert(1, stream_regs.name)
                strs_arr.insert(2, f"DTSUTC:{sysUtils.dts_utc()}")
                strs_arr.insert(3, f"PATH:{meter.syspath}")
                self.redops.pub_read_on_sec("MODBUS", "|".join(strs_arr))
