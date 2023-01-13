754b2d5fda9a41011633f85caa3ad35485a5bd1f
erik <erik@iotech.systems>
2023-01-13 11:54:24 +0100

    fix read delay

--- modbus/modbusMeterV1.py
+++ modbus/modbusMeterV1.py
@@ -121,12 +121,17 @@ class modbusMeterV1(object):
       return True
 
    def reads_to_str(self):
+      # -- -- -- --
+      if len(self.stream_reads) == 0:
+         return "[]"
+      # -- -- -- --
       buff: [] = []
       for mr in self.stream_reads:
          mr: meterReading = mr
          buff.append(f"{mr.regName}:{mr.regVal}")
       s = "|".join(buff)
       return f"[{s}]"
+      # -- -- -- --
 
    """
       private ... kinda
