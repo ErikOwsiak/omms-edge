507de082f9cedacbb8aa99d338da2e1082f0ce68
erik <erik@iotech.systems>
2023-02-12 06:14:54 +0100

    added last_read to redis

--- modbus/modbusRedisRelay.py
+++ modbus/modbusRedisRelay.py
@@ -109,8 +109,8 @@ class modbusRedisRelay(_th.Thread):
                no_pong_counter += 1
             # -- -- -- -- -- -- -- --
             minfo: str = str(meter.m_info)
-            _d = {"init_dts_utc": sysUtils.dts_utc(), "INIT_PING": msg,
-               meter.m_info.red_key: minfo}
+            _d = {"INIT_DTS": sysUtils.dts_utc(with_tz=True)
+               , "INIT_PING": msg, meter.m_info.red_key: minfo}
             self.redops.save_meter_data(meter.syspath, _dict=_d, delold=True)
             # -- -- -- -- -- -- -- --
          except Exception as e:
